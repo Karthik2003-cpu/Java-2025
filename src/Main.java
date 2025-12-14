ed1b9d66de6557f6c1eac0991f0320f2b5e079c2
Karthik2003-cpu <karthikrckm2003@gmail.com>
2025-12-14 13:45:46 +0530

    this is new scanner code

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void print(String str){
-        System.out.println("this is not static block: "+str);
-    }
     public static void main(String [] args){
 
-        //This is first program
-        /*
-            This is
-            a multi line comment
-         */
-
-        System.out.print("I like pizza\n");
-        System.out.println("It is really good!");
-        Main m = new Main();
-        m.print("Karthik");
-
-        //variables
-
-        int a=10,b=20;
-        System.out.println(a+" "+b);
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        sc.nextLine();
+        sc.close();
     }
 }
 
